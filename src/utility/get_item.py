e11368ab6f463df13e51861ed4c8abb73d17d8ac
mdmansooralam <mdmansooralam@outlook.in>
2024-11-04 14:38:40 +0530

    full clean

--- src/utility/get_item.py
+++
@@ -1,9 +0,0 @@
-
-from src.database.collections.item import Item
-
-def get_item(name):
-    for item in Item().items:
-        if(item['name'] == name):
-            return item
-    else:
-        return False

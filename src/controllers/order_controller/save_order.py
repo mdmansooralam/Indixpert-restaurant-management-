887e782aadb7d33661111a7f0cce57ddb5126f9d
mdmansooralam <mdmansooralam@outlook.in>
2024-11-05 14:18:10 +0530

    added try except

--- src/controllers/order_controller/save_order.py
+++ src/controllers/order_controller/save_order.py
@@ -11,43 +11,46 @@ from src.database.collections.default import Default
 
 
 def finalize_order(order):
-    ORDER = Order()
-    ITEM = Item()
-    name = validate_name(input('Customer Name : '))
-    if(not name):
-        print('please enter a valid name')
-        return
-    
-    mobile_no = validate_mobile(input('Customer Mobile Number : '))
+    try:
+        ORDER = Order()
+        ITEM = Item()
+        name = validate_name(input('Customer Name : '))
+        if(not name):
+            print('please enter a valid name')
+            return
+        
+        mobile_no = validate_mobile(input('Customer Mobile Number : '))
 
-    if(not mobile_no):
-        print('please enter a valid mobile number')
-        return
-    else:
-        total = 0
-        if order:
-            for order_item in order:
-                for item in ITEM.items:
-                    if(item['name'] == order_item['name']):
-                        item['quantity'] -= order_item['quantity']
-                        #add total
-                        total += item['sale_price'] * order_item['quantity']
-                        ITEM.save_item()  
-
-            id = str(uuid.uuid4())[:4].upper()
-            order_date = date.today().strftime('%d-%m-%Y')
-            create_by = UserState().get_state()['email']
-            status = 'process'
-            tax_percent = Default().tax_percent
-            tax = total / 100 * tax_percent
-            discount = 0
-            grand_total = total + tax - discount
-
-            new_order = OrderModel(id,name, mobile_no, order_date, order, total, create_by, status, tax, tax_percent, discount, grand_total).__dict__
-            ORDER.orders.append(new_order)
-            ORDER.save_order()
-            print('order save successful')
-            return new_order['id']
-    
+        if(not mobile_no):
+            print('please enter a valid mobile number')
+            return
         else:
-            print('No items in your order to save')
+            total = 0
+            if order:
+                for order_item in order:
+                    for item in ITEM.items:
+                        if(item['name'] == order_item['name']):
+                            item['quantity'] -= order_item['quantity']
+                            #add total
+                            total += item['sale_price'] * order_item['quantity']
+                            ITEM.save_item()  
+
+                id = str(uuid.uuid4())[:4].upper()
+                order_date = date.today().strftime('%d-%m-%Y')
+                create_by = UserState().get_state()['email']
+                status = 'process'
+                tax_percent = Default().tax_percent
+                tax = total / 100 * tax_percent
+                discount = 0
+                grand_total = total + tax - discount
+
+                new_order = OrderModel(id,name, mobile_no, order_date, order, total, create_by, status, tax, tax_percent, discount, grand_total).__dict__
+                ORDER.orders.append(new_order)
+                ORDER.save_order()
+                print('order save successful')
+                return new_order['id']
+        
+            else:
+                print('No items in your order to save')
+    except Exception as error:
+        print(error)

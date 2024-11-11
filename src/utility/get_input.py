21fd59cfed9e9b8831d1a953e3a4cf99475a8de4
mdmansooralam <mdmansooralam@outlook.in>
2024-11-11 14:53:34 +0530

    some chages in error loggging

--- src/utility/get_input.py
+++ src/utility/get_input.py
@@ -1,12 +1,17 @@
-
+from src.utility.log_error import LogError
 
 def get_input(validator, msg, err_msg):
     while True:
-        user_input = input(msg)
-        if(user_input.lower() == 'exit'):
-            return False
-        result = validator(user_input)
-        if(result):
-            return result
-        else:
-            print(err_msg)
+        try:
+            user_input = input(msg)
+            if(user_input.lower() == 'exit'):
+                return False
+            result = validator(user_input)
+            if(result):
+                return result
+            else:
+                # print(err_msg)
+                raise Exception(err_msg)
+        except Exception as error:
+            print(error)
+            LogError().err.exception(user_input)
